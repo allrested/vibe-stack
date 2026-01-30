6cd139f760ec36783889c7bcd4880b72c8d72009
Halil BARIM <halilbarim@192.168.1.113>
2026-01-30 23:13:28 +0300

    fix token error

--- agents/moltbot/start-moltbot.sh
+++ agents/moltbot/start-moltbot.sh
@@ -10,4 +10,10 @@ chown -R node:node /home/node/.openclaw /home/node/clawd /home/node
 # Start OpenClaw as node user
 echo "--- Starting OpenClaw ---"
 # Utilizing su to Switch User since we are root
-exec su - node -c "openclaw gateway --bind lan --allow-unconfigured"
+# Pass the token from environment variable if set
+TOKEN_ARG=""
+if [ -n "$CLAWDBOT_GATEWAY_TOKEN" ]; then
+  TOKEN_ARG="--token $CLAWDBOT_GATEWAY_TOKEN"
+fi
+
+exec su - node -c "openclaw gateway --bind lan --allow-unconfigured $TOKEN_ARG"
